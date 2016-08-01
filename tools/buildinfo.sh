1b0b5381c69266a56c1adf85f85ec6271170cf73
Aditya Upreti <aupreti72@gmail.com>
2016-08-01 05:32:19 -0700

    Build info.sh: Toxic_Device

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -61,4 +61,6 @@ if [ "$TARGET_UNIFIED_DEVICE" == "" ] ; then
 fi
 echo "ro.build.characteristics=$TARGET_AAPT_CHARACTERISTICS"
 
+echo "ro.toxic.device=$TOXIC_DEVICE"
+ 
 echo "# end build properties"
