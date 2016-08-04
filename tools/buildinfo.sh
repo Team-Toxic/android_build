7232e15d82ffb50ad47f241a001fc79ad80bd2e0
Aditya Upreti <aupreti72@gmail.com>
2016-08-04 08:13:59 -0700

    Revert "Buildinfo.sh Remove duplicate ro.toxic"
    This reverts commit 351cd17.

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -20,6 +20,9 @@ echo "ro.build.user=$USER"
 echo "ro.build.host=`hostname`"
 echo "ro.build.tags=$BUILD_VERSION_TAGS"
 echo "ro.build.flavor=$TARGET_BUILD_FLAVOR"
+# This needs to be defined somewhere		
+echo "ro.toxic.version=$BUILD_TOXIC_VERSION"		
+
 echo "ro.product.brand=$PRODUCT_BRAND"
 echo "ro.product.name=$PRODUCT_NAME"
 echo "ro.product.board=$TARGET_BOOTLOADER_BOARD_NAME"
